dfd013a43122ded4a079fffc37a73efe50ec9948
Antonio Piccolboni <antonio@piccolboni.info>
2015-10-15 12:36:19 -0700

    complete reboot around two equally important classes RegEx and CharClass

--- pkg/R/regez.R
+++ pkg/R/regez.R
@@ -1,98 +1,124 @@
-errfun =
-  function(e)
-    stop(
-      strsplit(
-        as.character(e),
-        'Error in grep\\(x, \\"\\"\\): ')[[1]][-1])
+errfun = function(e) stop(strsplit(as.character(e), ': ')[[1]][-1])
 
-assert.valid.regex =
-  function(x)
-    tryCatch(
-      is.rx(x) || is.integer(grep(x, "")),
-      error = errfun)
+assert =
+  function(x, p) {
+    tryCatch(stopifnot(p(x)), error = errfun)
+    x}
 
-is.rx = function(x) "rx" %in% class(x)
+escape =
+  function(s, is.meta)
+    paste(map_if(strsplit(s, "")[[1]], is.meta, ~paste0("\\", .)), collapse = "")
 
-metacharacters =
-  c(".", "\\",  "|", "(", ")", "[", "]", "{", "}", "^", "$", "*", "+", "?")
-is.metacharacter = function(x) x%in%metacharacters
-
-escape = function(x) rxq(paste0("\\", x))
-
-rx =
-  function(...) {
-    rxq(paste0(map(list(...), rx.one), collapse = ""))}
-
-rx.one = function(x) {UseMethod("rx.one")}
-
-rx.one.RX = function(x) {x}
-rx.one.character =
-  function(x){
-    x =
-      paste(
-        map_if(strsplit(x, "")[[1]], is.metacharacter, escape),
-        collapse = "")
-    rxq(x)}
-
-rxq =
-  function(x) {
-    assert.valid.regex(x)
+CharClass =
+  function(char.class)
     structure(
-      x,
-      class = "RX")}
+      assert.CharClass(char.class),
+      class = "CharClass")
 
-rangex =
-  function(x, y = NULL) {
-    stopifnot(is.null(y) || (nchar(x) == 1 && nchar(y) == 1))
-    rxq(
-      paste0(
-        "[",
-        rx(paste0(x, if(!is.null(y)) "-", y)),
-        "]"))}
+as.CharClass = function(x) UseMethod("as.CharClass")
+as.CharClass.character = function(x) CharClass(escape.CharClass(x))
+as.CharClass.CharClass = identity
 
-BEL = rx(escape("a"))
-ESC = rx(escape("e"))
-FF = rx(escape("f"))
-CR = rx(escape("r"))
-TAB = rx(escape("t"))
 
-char.class =
-  alphanumeric = rx("[:alnum:]")
-alphabetic = rx("[:alpha:]")
-blank = rx("[:blank:]")
-control = rx("[:cntrl:]")
-digit = rx("[:digit:]")
-graphical = rx("[:graph:]")
-lower = rx("[:lower:]")
-print = rx("[:print:]")
-punct = rx("[:punct:]")
-space = rx("[:space:]")
-upper = rx("[:upper:]")
-xdigit = rx("[:xdigit:]")
+assert.CharClass =
+  function(char.class)
+    assert(char.class, function(cc) is.integer(grep(paste("[", cc, "]"), "", perl = TRUE)))
 
-any = rx(".")
-word.char = rx(escape("w"))
-non.word.char = rx(escape("W"))
-digit = rx(escape("d"))
-space.char = rx(escape("s"))
-non.digit = rx(escape("D"))
-non.space.char = rx(escape("S"))
-line.begin = rx("^")
-line.end = rx("$")
-word.begin = rx(escape("<"))
-word.end = rx(escape(">"))
-word.edge = rx(escape("b"))
-not.word.edge = rx(escape("B"))
+escape.CharClass = function(s) escape(s, is.meta.CharClass)
 
-enclose = function(x) rxq(paste0("(", rx(x), ")"))
-optional = function(x) rx(enclose(x), "?")
-any.number.of = function(x) rx(enclose(x), "*")
-at.least.one = function(x) rx(enclose(x), "+")
-exactly.n = function(x, n) rx(enclose(x), "{", n, "}")
-at.least.n = function(x, n) rx(enclose(x), "{", n, ",}")
-a.range = function(x, n, m) rx(enclose(x), "{", n, ",", m, "}")
-greedy = function(x) rx(x, "?")
+is.meta.CharClass = function(c) c %in% c("\\", "-", "[", "]", "^")
+
+predef.CharClasses = cc =
+  map(
+    c(
+      alphanumeric = "[:alnum:]", alphabetic = "[:alpha:]",
+      blank = "[:blank:]",    control = "[:cntrl:]",
+      digit = "[:digit:]",    graphical = "[:graph:]",
+      lower = "[:lower:]",    print = "[:print:]",
+      punct = "[:punct:]",    space = "[:space:]",
+      upper = "[:upper:]",    xdigit = "[:xdigit:]"),
+    CharClass)
+
+char.range =
+  function(start.char, stop.char)
+    CharClass(paste0(start.char, "-", stop.char))
+
+RegEx =
+  function(regex)
+    structure(
+      assert.RegEx(regex),
+      class = "RegEx")
+
+as.RegEx = function(x) UseMethod("as.RegEx")
+as.RegEx.character = function(x) RegEx(escape.RegEx(x))
+as.RegEx.RegEx = identity
+
+assert.RegEx =
+  function(regex)
+    assert(regex, function(rx) is.integer(grep(rx, "", perl = TRUE)))
+
+escape.RegEx = function(s) escape(s, is.meta.RegEx)
+
+is.meta.RegEx =
+  function(c) {
+    c %in%
+      c(".", "\\",  "|", "(", ")", "[", "]", "{", "}", "^", "$", "*", "+", "?")}
+
+concat2  = `%+%`  = function(x, y) UseMethod("concat2")
+
+concat2.character =
+  function(x, y) {
+    switch(
+      class(y),
+      character = paste0(x, y),
+      CharClass = concat(as.CharClass(x), y),
+      RegEx = concat(as.RegEx(x), y))}
+
+concat2.CharClass =
+  function(x, y)
+    CharClass(paste0(as.CharClass(x), as.CharClass(y)))
+
+concat2.RegEx =
+  function(x, y)
+    RegEx(paste0(as.RegEx(x), as.RegEx(y)))
+
+concat =
+  function(...) {
+    args = list(...)
+    if(length(args) == 2)
+      do.call(concat2, args)
+    else
+      concat2(args[[1]], do.call(concat, args[-1]))}
+
+escape.seq = es =
+  map(
+    c(
+      BEL = "a", ESC = "e", FF = "f", CR = "r", TAB = "t",
+      word.char = "w",  non.word.char = "W", digit = "d",
+      space.char = "s", non.digit = "D",     non.space.char = "S",
+      word.edge = "b", not.word.edge = "B"),
+    function(x) RegEx(paste0("\\", x)))
+
+anychar = RegEx(".")
+line.begin = RegEx("^")
+line.end = RegEx("$")
+
+build.RegEx = function(...) RegEx(paste0(list(...), collapse = ""))
+
+
+any.of = function(cc) build.RegEx("[", as.CharClass(cc), "]")
+none.of = function(cc) build.RegEx("[^", as.CharClass(cc), "]")
+
+enclose = function(rx) build.RegEx("(", as.RegEx(rx), ")")
+
+wildcard = function(rx, ...) build.RegEx(enclose(as.RegEx(rx)), paste0(...))
+
+optional =  function(x)  wildcard(x, "?")
+any.number.of = function(x) wildcard(x, "*")
+at.least.one = function(x) wildcard(x, "+")
+exactly.n = function(x, n) wildcard(x, "{", n, "}")
+at.least.n = function(x, n) wildcard(x, "{", n, ",}")
+range.of = function(x, n, m) wildcard(x, "{", n, ",", m, "}")
+
 or = `%|%` =
-  function(x, y) rx(enclose(x), "|", enclose(y))
-
-word = rx(word.begin, at.least.one(word.char), word.end)
+  function(rxl, rxr) build.RegEx(enclose(as.RegEx(rxl)), "|", enclose(as.RegEx(rxr)))
